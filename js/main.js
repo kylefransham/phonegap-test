c02cf84a27f9a88bf98a3c9d9fa9f29e4c851c13
Kyle Fransham <fransham@kyle-laptop.(none)>
2013-06-22 21:56:08 -0400

    testing notification

--- js/main.js
+++ js/main.js
@@ -4,16 +4,48 @@ var app = {
         this.homeTpl = Handlebars.compile($("#home-tpl").html());
         this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
 	var self = this;
+	this.registerEvents();
+
+        this.detailsURL = /^#employees\/(\d{1,})/;
         this.store = new MemoryStore(function() {        
-	    $('body').html(new HomeView(self.store).render().el);
+	    self.route();
         });
     },
 
-    showAlert: function (message, title) {
-        if (navigator.notification) {
-            navigator.notification.alert(message, null, title, 'OK');
+    registerEvents: function() {
+        var self = this;
+	$(window).on('hashchange', $.proxy(this.route, this));
+        // Check of browser supports touch events...
+        if (document.documentElement.hasOwnProperty('ontouchstart')) {
+            // ... if yes: register touch event listener to change the "selected" state of the item
+            $('body').on('touchstart', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('touchend', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
         } else {
-            alert(title ? (title + ": " + message) : message);
+             // ... if not: register mouse events instead
+            $('body').on('mousedown', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('mouseup', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        }
+    },
+
+    route: function() {
+        var hash = window.location.hash;
+        if (!hash) {
+            $('body').html(new HomeView(this.store).render().el);
+            return;
+        }
+        var match = hash.match(app.detailsURL);
+        if (match) {
+            this.store.findById(Number(match[1]), function(employee) {
+                $('body').html(new EmployeeView(employee).render().el);
+            });
         }
     }
 
