5894b46962a9ca70316ea3972c7c0a8db635fe9e
Kyle Fransham <fransham@kyle-laptop.(none)>
2013-06-22 21:36:41 -0400

    testing notification

--- js/main.js
+++ js/main.js
@@ -1,24 +1,12 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
-
     initialize: function() {
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
 	var self = this;
-        this.store = new MemoryStore(function() {
-            self.showAlert('Store Initialized', 'Info');
+        this.store = new MemoryStore(function() {        
+	    $('body').html(new HomeView(self.store).render().el);
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
 
     showAlert: function (message, title) {
@@ -27,7 +15,7 @@ var app = {
         } else {
             alert(title ? (title + ": " + message) : message);
         }
-    },
+    }
 
 };
 
